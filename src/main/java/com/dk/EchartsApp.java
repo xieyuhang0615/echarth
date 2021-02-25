b833a3131710e6d2f4726ec0c3f4c945a5b75b15
Administrator <499165324@qq.com>
2021-02-26 00:23:28 +0800

    测试

---
+++ src/main/java/com/dk/EchartsApp.java
@@ -0,0 +1,11 @@
+package com.dk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchartsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EchartsApp.class,args);
+    }
+}
